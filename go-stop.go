79d0ce51189cd90a5f8712802e98a1bffcf31b6b
Cam <cirmas@protonmail.com>
2019-01-04 08:44:05 -0800

    Add basic Postgres connection

--- go-stop.go
+++ go-stop.go
@@ -1,9 +1,20 @@
 package main
 
 import (
+    "database/sql"
 	"fmt"
+    "log"
+    _ "github.com/lib/pq"
 )
 
 func main() {
 	fmt.Println("Hello Go-Stop")
+
+    connStr := "user=postgres password=postgres dbname=go_stop_go sslmode=verify-full"
+    _, err := sql.Open("postgres", connStr)
+    if err != nil {
+        log.Fatal(err)
+    } else {
+        log.Println("Connected to postgres")
+    }
 }
