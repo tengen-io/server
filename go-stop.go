aa608aa5a052026a5481eb669ca296b1fbff5e1e
Cam <cirmas@protonmail.com>
2019-01-04 09:05:41 -0800

    Add basic HTTP server + postgres

--- go-stop.go
+++ go-stop.go
@@ -1,20 +1,47 @@
+/*
+Server implementation of the board game Go.
+*/
 package main
 
 import (
-    "database/sql"
-	"fmt"
-    "log"
-    _ "github.com/lib/pq"
+	"database/sql"
+	// "fmt"
+	_ "github.com/lib/pq"
+	"log"
+	"net/http"
 )
 
-func main() {
-	fmt.Println("Hello Go-Stop")
+type Server struct {
+	db *sql.DB
+}
 
-    connStr := "user=postgres password=postgres dbname=go_stop_go sslmode=verify-full"
-    _, err := sql.Open("postgres", connStr)
-    if err != nil {
-        log.Fatal(err)
-    } else {
-        log.Println("Connected to postgres")
-    }
+func (s *Server) Start() {
+	db, err := connectToDB()
+	if err != nil {
+		log.Fatal(err)
+		return
+	} else {
+		log.Println("Connected to postgres")
+	}
+
+	s.db = db
+	log.Println("Listening on http://localhost:8000")
+	log.Fatal(http.ListenAndServe(":8000", s))
+}
+
+func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		w.Write([]byte("Hello"))
+	}
+}
+
+func connectToDB() (*sql.DB, error) {
+	connStr := "user=postgres password=postgres dbname=go_stop_go sslmode=verify-full"
+	return sql.Open("postgres", connStr)
+}
+
+func main() {
+	s := Server{}
+	s.Start()
 }
