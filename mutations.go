941de852ccb9f16e397861e68b9989159957d5cc
Infinytum <43315617+infinytum@users.noreply.github.com>
2019-01-23 23:51:35 +0100

    [resolves #16] Split up schema definition
    * Split up schema definition
    
    * Fixed assingment and imports

---
+++ mutations.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"github.com/camirmas/go_stop/resolvers"
+	"github.com/graphql-go/graphql"
+)
+
+var (
+	mutationType = graphql.NewObject(graphql.ObjectConfig{
+		Name: "Mutation",
+		Fields: graphql.Fields{
+			"createUser": &graphql.Field{
+				Type: tokenType,
+				Args: graphql.FieldConfigArgument{
+					"username": &graphql.ArgumentConfig{
+						Type: graphql.NewNonNull(graphql.String),
+					},
+					"email": &graphql.ArgumentConfig{
+						Type: graphql.NewNonNull(graphql.String),
+					},
+					"password": &graphql.ArgumentConfig{
+						Type: graphql.NewNonNull(graphql.String),
+					},
+					"passwordConfirmation": &graphql.ArgumentConfig{
+						Type: graphql.NewNonNull(graphql.String),
+					},
+				},
+				Resolve: resolvers.CreateUser,
+			},
+
+			"createGame": &graphql.Field{
+				Type: gameType,
+				Args: graphql.FieldConfigArgument{
+					"opponentUsername": &graphql.ArgumentConfig{
+						Type: graphql.NewNonNull(graphql.String),
+					},
+				},
+				Resolve: resolvers.CreateGame,
+			},
+
+			"pass": &graphql.Field{
+				Type: gameType,
+				Args: graphql.FieldConfigArgument{
+					"gameId": &graphql.ArgumentConfig{
+						Type: graphql.ID,
+					},
+				},
+				Resolve: resolvers.Pass,
+			},
+
+			"logIn": &graphql.Field{
+				Type: tokenType,
+				Args: graphql.FieldConfigArgument{
+					"username": &graphql.ArgumentConfig{
+						Type: graphql.NewNonNull(graphql.String),
+					},
+					"password": &graphql.ArgumentConfig{
+						Type: graphql.NewNonNull(graphql.String),
+					},
+				},
+				Resolve: resolvers.LogIn,
+			},
+
+			"addStone": &graphql.Field{
+				Type: gameType,
+				Args: graphql.FieldConfigArgument{
+					"gameId": &graphql.ArgumentConfig{
+						Type: graphql.NewNonNull(graphql.ID),
+					},
+					"x": &graphql.ArgumentConfig{
+						Type: graphql.NewNonNull(graphql.Int),
+					},
+					"y": &graphql.ArgumentConfig{
+						Type: graphql.NewNonNull(graphql.Int),
+					},
+				},
+				Resolve: resolvers.AddStone,
+			},
+		},
+	})
+)
